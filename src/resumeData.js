1da6e009b6a8689eabc63ac136144e4e48aba13d
Zeeshan Raza <zeeshanraza@admins-MacBook-Pro-5.local>
2022-06-15 16:55:38 +0500

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,122 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/mzraz",
+    "name": "Zeeshan Raza",
+    "role": "Frontend Developer and Automation Engineer",
+    // "linkedinId":"https://www.linkedin.com/in/zeeshan-raza-aa877b18b/",
+    "EmailId":"mzraza512@gmail.com",
+    "skypeid": "mzraza512@gmail.com",
+    "contactNumber" : "03004577594",
+    "facebook":"https://www.facebook.com/zeeshan.shah.92102",
+    "roleDescription": " I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games specially Counter Strike Golbal Offensive(CSGO) in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/zeeshan-raza-aa877b18b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/mzraz",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/Zeeshan12183031",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am graduate of BS Computer Science from UET Lahore. Currently, I am working as an automation Engineer in StarzPlay Arabia and want to become a fulltime developer. I am a self taught Full Stack Web Developer, currently diving deeper into Web development. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Pakistan, Punjab, Lahore",
+    "website":"mzraza512@gmail.com",
+    // "website":"03004577594",
+    "contactNumber":"03004577594",
+    "education":[
+      {
+        "UniversityName":"University of Engineering and Technology(Lahore)",
+        "specialization":"Graduation",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"KIPS College",
+        "specialization":"FSc Pre-engineering",
+        "MonthOfPassing":"may",
+        "YearOfPassing":"2016",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Bitsclan IT Solutions",
+        "Designation":"SQA Engineer",
+        "specialization":"",
+        "MonthOfLeaving":"Aug 2020 - ",
+        "YearOfLeaving":"May 2021",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"StarzPlay Arabia",
+        "Designation":"Automation Engineer",
+        "specialization":"",
+        "MonthOfLeaving":"May 2021 - ",
+        "YearOfLeaving":"Present",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"My skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"CSGO"
+      },
+      {
+        "skillname":"illustrator"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
